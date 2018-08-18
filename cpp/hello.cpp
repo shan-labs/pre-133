6cf5bf15809be0b2eff25a7dd1999eaa262f04b1
Khinshan Khan <khanshan501@gmail.com>
2018-08-18 17:29:24 -0400

    hello world... from cpp

---
+++ cpp/hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cout << "Hello World!" << endl;
+
+  for(int i = 0; i <= 5; i++){
+    if(i == 5){
+      cout << i <<endl;
+    }else{
+      cout << i << ", " ;
+    }
+  }
+
+  return 0;
+}
